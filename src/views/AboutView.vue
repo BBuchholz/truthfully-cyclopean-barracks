199f2cd40dab2af852a00c9343c2b24b0d20d16f
Brent Buchholz <brentbuchholz@gmail.com>
2023-03-29 10:10:17 -0500

    First commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <img
+      alt="A Technician In A Cyberpunk Occult Style Working On An Engine"
+      src="../assets/Imagine_Occult-Technician_with-stamp.jpg"
+    /> -->
+    <AboutLinks />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AboutLinks from "@/components/AboutLinks.vue"; // @ is an alias to /src
+export default defineComponent({
+  name: "AboutView",
+  components: {
+    AboutLinks,
+  },
+});
+</script>
